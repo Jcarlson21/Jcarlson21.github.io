10df2707fa490bfc972f3397ba038cd93508d799
jakecarlson <jake.f.carlson4@gmail.com>
2023-09-01 14:37:30 -0400

    trying somthing

---
+++ scripts.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const helloWorld = document.getElementById("hello-world");
+
+    helloWorld.addEventListener("click", function () {
+        // Animate the "Hello World" element
+        helloWorld.style.transform = "scale(2)";
+        helloWorld.style.opacity = "0";
+
+        // Add a delay for the animation before redirecting to your full website
+        setTimeout(function () {
+            window.location.href = "your-full-website.html";
+        }, 500); // Adjust the delay time (in milliseconds) as needed
+    });
+});
